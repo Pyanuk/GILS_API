7593630846044ed43ea557cbec95127f19f17dcb
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2025-06-03 13:27:08 +0300

    Create ArtistSimpleDto.cs

---
+++ Models/DTOs/ArtistSimpleDto.cs
@@ -0,0 +1,8 @@
+namespace SoundPlayerAPI.Models.DTOs
+{
+    public class ArtistSimpleDto
+    {
+        public Guid ArtistId { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
